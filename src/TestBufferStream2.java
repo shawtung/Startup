d696aa309c9d44c67dd9bc804f7cc052e98df433
shawtung <shawtung@qq.com>
2015-09-15 16:51:58 +0800

    Merge branch 'master' of https://github.com/shawtung/Startup
    Conflicts:
    	src/JDBCFun2.java

--- src/TestBufferStream2.java
+++ src/TestBufferStream2.java
@@ -1,37 +1,50 @@
-import java.io.*;
-
 /**
  * Created by Shaw on 2015/9/15.
  */
+
+import java.io.*;
+
 public class TestBufferStream2 {
 
     public static void main(String[] args) {
+
         try {
+            //Declaration
             File file = new File("d:/1.txt");
-            file.createNewFile();
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            FileReader fr = new FileReader(file);
+            FileWriter fw = new FileWriter(file);
+            BufferedReader br = new BufferedReader(fr);
+            BufferedWriter bw = new BufferedWriter(fw);
             String s;
+
+            //Create file
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            //Write-in contents
             for (int i = 1; i <= 100; i++) {
                 s = String.valueOf(Math.random());
                 bw.write(s);
                 bw.newLine();
             }
             bw.flush();
+
+            //Print contents
             while ((s = br.readLine()) != null) {
                 System.out.println(s);
             }
+
+            //Clean-up
             bw.close();
             br.close();
-
-
+            fw.close();
+            fr.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
     }
-
 
 }
